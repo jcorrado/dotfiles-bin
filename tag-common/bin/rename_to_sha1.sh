1543755a6753f00e3528402394bf2b7f4bffbe78
Jereme Corrado <jereme@zoion.net>
2018-08-27 09:02:25 -0400

    Moved rename_to_sha to zsh fn

--- tag-common/bin/rename_to_sha1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-IFS=$'\n'
-
-for f in $(find . -maxdepth 1 -type f); do
-    ext=$(echo $f | perl -ne '/\.([^.]+)$/ && print $1')
-    sha1=$(sha1sum $f | perl -ae 'print $F[0]')
-    mv -v "$f" "${sha1}.${ext}"
-done

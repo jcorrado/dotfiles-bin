7ec8f7c4dfe6d8d114447a8eedfde3a2c54c2803
Jereme Corrado <jereme@zoion.net>
2018-09-12 17:40:28 -0400

    shorten_path.pl bug fix

--- tag-common/bin/shorten_path.pl
+++ tag-common/bin/shorten_path.pl
@@ -14,7 +14,8 @@ while (length $line > $max_len) {
     if ($line =~ m!/[^/]{2,}/!) {
         $line =~ s!/([^/])[^/]+!/$1!;
     } else {
-        my $dir_len = length $line =~ m!/([^/]+)$!;
+        my ($dir) = $line =~ m!/([^/]+)$!;
+        my $dir_len = length $dir;
         my $len = $dir_len > $max_len ? $dir_len : $max_len;
         $line =~ s/^(.).+(.{$len,$len})$/$1..$2/;
         last;

4a7f8d7a64e0a77279d53ea4ac9907e2e3aeb890
Jereme Corrado <jereme@zoion.net>
2018-12-19 12:45:24 -0500

    Added mutt_for_msgid.sh to launch a mutt for a passed Message-ID

---
+++ tag-common/bin/mutt_for_msgid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+WINDOW_POSITION="113x32+2141+149"
+
+msgid="$1"
+if test -z $msgid; then
+    echo please supply a Message-ID
+    exit 1
+fi
+
+maildir=$(mktemp -d)
+
+mu find -u --format=links --clearlinks --linksdir=$maildir "i:$msgid"
+xfce4-terminal --geometry=$WINDOW_POSITION --command="zsh -ic \"mutt -e 'set quit=yes' -f $maildir\""

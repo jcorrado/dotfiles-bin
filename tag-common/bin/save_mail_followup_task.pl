2e891445f6e79f1865e87035d305705d5e52e6fa
Jereme Corrado <jereme@zoion.net>
2018-12-20 14:25:04 -0500

    Quick cleanup of email subjects to avoid breaking org links

--- tag-common/bin/save_mail_followup_task.pl
+++ tag-common/bin/save_mail_followup_task.pl
@@ -12,7 +12,7 @@ getopts ('f:t:');
 die "Please supply a file, and optionally a tags string\n" unless $opt_f ;
 
 my $org_task_file = $opt_f;
-my $org_tags = $opt_t or ':\@email:';
+my $org_tags = $opt_t;
 
 my $msg;
 {
@@ -25,9 +25,9 @@ open my $fh, ">> $org_task_file"
     or die "couldn't open $org_task_file for append: $!\n";
 my $email = Email::Simple->new($msg);
 
-my $message_id = $email->header('Message-Id');
-$message_id =~ s/^<//;
-$message_id =~ s/>$//;
+my $message_id = clean_message_id($email->header('Message-Id'));
+my $subject = clean_str_for_org_link($email->header('Subject'));
+my $from = clean_str_for_org_link($email->header('From'));
 
 my $new_task = sprintf
     "* NEXT Followup: [[message-id:%s][%s - %s]]  %s\n" .
@@ -35,7 +35,7 @@ my $new_task = sprintf
     " :Created: [%s]\n" .
     " :END:\n" .
     "Message Date: %s\n\n",
-    $message_id, $email->header('From'), $email->header('Subject'), $org_tags,
+    $message_id, $from, $subject, $org_tags,
     curr_org_ts(),
     $email->header('Date');
 
@@ -52,4 +52,17 @@ sub curr_org_ts {
     $year += 1900;
     my @wdays = qw(Sun Mon Tue Wed Thu Fri Sat);
     return sprintf '%d-%02d-%02d %s %02d:%02d', $year, $mon, $mday, $wdays[$wday], $hour, $min;
+}
+
+sub clean_message_id {
+    my $msg_id = shift;
+    $msg_id =~ s/^<//;
+    $msg_id =~ s/>$//;
+    $msg_id;
+}
+
+sub clean_str_for_org_link {
+    my $str = shift;
+    $str =~ s![\[\]]! !g;
+    $str;
 }

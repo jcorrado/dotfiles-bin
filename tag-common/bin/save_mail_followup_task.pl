49914f5a67000b077c001e85303badecef560d46
Jereme Corrado <jereme@zoion.net>
2018-12-21 17:16:16 -0500

    save_mail_followup_task.pl now using org-protocol interface

--- tag-common/bin/save_mail_followup_task.pl
+++ tag-common/bin/save_mail_followup_task.pl
@@ -6,13 +6,19 @@ use strict;
 use Email::Simple;
 use Getopt::Std;
 
-our ($opt_f, $opt_t);
-getopts ('f:t:');
+# map a category to a capture template (to define the refile target)
+# and a string of tags
 
-die "Please supply a file, and optionally a tags string\n" unless $opt_f ;
+my %CATEGORIES = (
+    personal => { template => 'm', tags =>  'personal' },
+    birchbox => { template => 'n', tags =>  'birchbox' });
 
-my $org_task_file = $opt_f;
-my $org_tags = $opt_t;
+our ($opt_c);
+getopts ('c:');
+
+my $cat = $opt_c;
+my $org_template = $CATEGORIES{$cat}{template} || $CATEGORIES{personal}{template};
+my $org_tags = $CATEGORIES{$cat}{tags} || $CATEGORIES{personal}{tags};
 
 my $msg;
 {
@@ -21,38 +27,25 @@ my $msg;
 }
 exit if $msg =~ /^\n?$/;
 
-open my $fh, ">> $org_task_file"
-    or die "couldn't open $org_task_file for append: $!\n";
 my $email = Email::Simple->new($msg);
 
 my $message_id = clean_message_id($email->header('Message-Id'));
 my $subject = clean_str_for_org_link($email->header('Subject'));
 my $from = clean_str_for_org_link($email->header('From'));
 
-my $new_task = sprintf
-    "* NEXT Followup: [[message-id:%s][%s - %s]]  %s\n" .
-    " :PROPERTIES:\n" .
-    " :Created: [%s]\n" .
-    " :END:\n" .
-    "Message Date: %s\n\n",
-    $message_id, $from, $subject, $org_tags,
-    curr_org_ts(),
-    $email->header('Date');
+my $org_protocol_uri = sprintf(
+    'org-protocol://capture?template=%s&url=message-id:%s&title=%s-%s&body=%s',
+    $org_template,
+    $message_id,
+    $from,
+    $subject,
+    $org_tags);
 
-print $fh $new_task;
-close $fh;
-
-print "task added to $org_task_file\n\n$new_task";
+unless (system("emacsclient '$org_protocol_uri'") == 0) {
+    die "fork for emacsclient failed: $!\n";
+}
 
 exit 0;
-
-# Return current ts in org format: 2014-05-12 Mon 08:51
-sub curr_org_ts {
-    my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);
-    $year += 1900;
-    my @wdays = qw(Sun Mon Tue Wed Thu Fri Sat);
-    return sprintf '%d-%02d-%02d %s %02d:%02d', $year, $mon, $mday, $wdays[$wday], $hour, $min;
-}
 
 sub clean_message_id {
     my $msg_id = shift;
@@ -63,6 +56,6 @@ sub clean_message_id {
 
 sub clean_str_for_org_link {
     my $str = shift;
-    $str =~ s![\[\]]! !g;
+    $str =~ s!['"\[\]]! !g;
     $str;
 }

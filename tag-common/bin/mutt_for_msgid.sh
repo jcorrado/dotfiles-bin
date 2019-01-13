66326aef36964fe74c598b8dccd00fb8c8e8d2b5
Jereme Corrado <jereme@zoion.net>
2019-01-12 19:21:35 -0500

    Org, mutt: added org-link support

--- tag-common/bin/mutt_for_msgid.sh
+++ tag-common/bin/mutt_for_msgid.sh
@@ -12,5 +12,5 @@ fi
 
 maildir=$(mktemp -d)
 
-mu find -u --format=links --clearlinks --linksdir=$maildir "i:$msgid"
+mu find -ru --format=links --clearlinks --linksdir=$maildir "i:$msgid"
 xfce4-terminal --geometry=$WINDOW_POSITION --command="zsh -ic \"mutt -e 'set quit=yes' -f $maildir\""
--- tag-common/bin/save_mail_followup_task.pl
+++ tag-common/bin/save_mail_followup_task.pl
@@ -1,22 +1,24 @@
 #!/usr/bin/perl
 
+# Create an org-mode task (default) or enqueue a link.
+
 use warnings;
 use strict;
 
 use Email::Simple;
 use Getopt::Std;
 
-# map a category to a capture template (to define the refile target)
+# Map a category to a capture template (to define the refile target)
 # and a string of tags
-
 my %CATEGORIES = (
     personal => { template => 'mp', tags =>  '' },
     birchbox => { template => 'mb', tags =>  '' });
 
-our ($opt_c);
-getopts ('c:');
+our ($opt_c, $opt_l);
+getopts ('c:l');
 
 my $cat = $opt_c;
+my $do_link = $opt_l;
 my $org_template = $CATEGORIES{$cat}{template} || $CATEGORIES{personal}{template};
 my $org_tags = $CATEGORIES{$cat}{tags} || $CATEGORIES{personal}{tags};
 
@@ -33,13 +35,22 @@ my $message_id = clean_message_id($email->header('Message-Id'));
 my $subject = clean_str_for_org_link($email->header('Subject'));
 my $from = clean_str_for_org_link($email->header('From'));
 
-my $org_protocol_uri = sprintf(
-    'org-protocol://capture?template=%s&url=message-id:%s&title=%s-%s&body=%s',
-    $org_template,
-    $message_id,
-    $from,
-    $subject,
-    $org_tags);
+my $org_protocol_uri;
+if ($do_link) {
+    $org_protocol_uri = sprintf(
+        'org-protocol://store-link?url=message-id:%s&title=%s-%s',
+        $message_id,
+        $from,
+        $subject);
+} else {
+    $org_protocol_uri = sprintf(
+        'org-protocol://capture?template=%s&url=message-id:%s&title=%s-%s&body=%s',
+        $org_template,
+        $message_id,
+        $from,
+        $subject,
+        $org_tags);
+}
 
 unless (system("emacsclient '$org_protocol_uri'") == 0) {
     die "fork for emacsclient failed: $!\n";

fa4e4ee61629de226a7db9a49da3a31c4afc2448
Jereme Corrado <jereme@zoion.net>
2019-01-19 14:53:58 -0500

    org-mode-connector.rb improvements, included shortened title

--- tag-common/bin/org-mode-connector.rb
+++ tag-common/bin/org-mode-connector.rb
@@ -6,16 +6,15 @@ require 'erb'
 org_link_uri = "org-protocol://store-link?url=message-id:%s&title=%s"
 org_capture_uri = "org-protocol://capture?url=message-id:%s&title=%s&template=%s"
 
-mail = Mail.read_from_string($stdin.read)
 template = ARGV.shift
+mail = Mail.read_from_string($stdin.read)
+message_id = ERB::Util::url_encode(mail.message_id)
+title =  ERB::Util::url_encode("#{mail.from.first}: #{mail.subject}"[0..55])
 
 if (template)
-  org_protocol_uri = org_capture_uri % [ERB::Util::url_encode(mail.message_id),
-                                        ERB::Util::url_encode("#{mail.from.first}: #{mail.subject}"),
-                                        template]
+  org_protocol_uri = org_capture_uri % [message_id, title, template]
 else
-  org_protocol_uri = org_link_uri % [ERB::Util::url_encode(mail.message_id),
-                                     ERB::Util::url_encode("#{mail.from.first}: #{mail.subject}")]
+  org_protocol_uri = org_link_uri % [message_id, title]
 end
 
 system("emacsclient", org_protocol_uri) or

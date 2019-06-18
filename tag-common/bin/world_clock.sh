f25c316f7d4a9d9b092578111d4a76f374e07bc1
Jereme Corrado <jereme@zoion.net>
2019-06-18 09:55:05 -0400

    Small world_clock formatting changes

--- tag-common/bin/world_clock.sh
+++ tag-common/bin/world_clock.sh
@@ -3,8 +3,10 @@
 #DATE_FMT='+%Y-%m-%d %H:%M %:z %Z'
 DATE_FMT='+%a, %b %d, %Y %H:%M %:z %Z'
 
+echo -n "NYC "; TZ=America/New_York date "$DATE_FMT"
 echo -n "UTC "; TZ=UTC date "$DATE_FMT"
-#echo -e
+echo -e
+echo -n "LAX "; TZ=America/Los_Angeles date "$DATE_FMT"
 echo -n "NYC "; TZ=America/New_York date "$DATE_FMT"
 echo -n "LDN "; TZ=Europe/London date "$DATE_FMT"
 echo -n "PAR "; TZ=Europe/Paris date "$DATE_FMT"

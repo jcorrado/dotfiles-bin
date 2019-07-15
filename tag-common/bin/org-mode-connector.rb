e5833702d5911ec26e335ce5d8737fb65ac5b0ae
Jereme Corrado <jereme@zoion.net>
2019-07-15 08:55:15 -0400

    Transitioning from Birchbox and AUAAP to Team Mobot

--- tag-common/bin/org-mode-connector.rb
+++ tag-common/bin/org-mode-connector.rb
@@ -20,9 +20,9 @@ end
 system("emacsclient", org_protocol_uri) or
   raise "failed to run emacsclient"
 
-# Our "mp" and "mb" templates (see `org-capture-templates') allow for
+# Our "mp" and "mt" templates (see `org-capture-templates') allow for
 # populating some details, so we move focus over to Emacs.
-if template == "mp" or template == "mb"
+if template == "mp" or template == "mt"
   sleep(1.0/3)
   system("wmctrl", "-a", "CAPTURE-refile.org") or
     raise "failed to run wmctrl"
